d5233cae9b0f733439946c3cc39cd78514235a3d
Francisco Sobrino <fsobrino@trileucosolutions.com>
2015-11-17 09:19:28 +0100

    Added html interface and form petitions

--- discovery_mars/app/controllers/pages_controller.rb
+++ discovery_mars/app/controllers/pages_controller.rb
@@ -4,6 +4,8 @@ class PagesController < ApplicationController
   end
 
   def track
+    land = Land.parse_order_from_text params[:track_orders] if params[:track_orders]
+    render json: land.try(:to_json, {include: :rovers})
   end
 
 end
